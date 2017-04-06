e257e1d534a9f4c465ff756b9492e87d44860dbf
bedelman3 <bedelman3@gatech.edu>
2017-04-06 14:30:52 -0400

    Add Data Point update
    Added dropdown

--- AddDataPoint.php
+++ AddDataPoint.php
@@ -6,7 +6,29 @@
         <center><h1>Add Data Point</h1></center>
     </div>
     
+    <a href="addLocation">add a new location</a>
+    <br>
+    <br>
+    <h3>POI Location Name: </h3>
+    <select>
+    <?php
     
-<!--Jack's comment-->    
+    $conn = mysql_connect("localhost","compuser","yeahsure");
+    mysql_select_db("4400DatabaseProject",$conn);
+
+
+    if (!$conn) {
+        echo "Unable to connect to DB: " . mysql_error();
+        exit;
+    }
+    
+    $theQuery = "SELECT locationName FROM ``";
+    $theResponse = mysql_query($theQuery);
+
+    while($row = mysql_fetch_assoc($theResponse)) {
+        echo "<option>".$row["locationName"]."</option>";
+    }
+    ?>
+    </select>
     
 </html>
