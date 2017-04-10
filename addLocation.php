2bc74016dcff50778af1333ef6b6cbb691b3f95d
Orestis Markozanes <Orestis Markozanes>
2017-04-10 12:48:06 -0400

    Still unfinished

---
+++ addLocation.php
@@ -0,0 +1,78 @@
+<html>
+
+	<head><link rel="stylesheet" type="text/css" href="main.css"></head>
+
+	<div>
+		<center><h1>Add a new Location</h1></center>
+	</div>
+
+	<?php  
+
+
+	?>
+
+	<form>
+		<h3>POI location name: </h3><br>
+		<input type = "text" name = "newLocation"><br>
+
+		<select name="City" required>
+		<?php 
+			$conn = mysql_connect("localhost","compuser","yeahsure");
+			mysql_select_db("4400DatabaseProject",$conn);
+			echo mysql_error();
+
+			if (!$conn) {
+				echo "Unable to connect to DB: " .mysql_error();
+				exit;
+			}
+
+			$theQuery = "select city FROM 'CityState'";
+			$theResponse = mysql_query($theQuery);
+
+			while($row  = mysql_fetch_assoc($theResponse)) {
+				echo "<option>".$row["city"]."</option>";
+			}
+		?>
+			
+		</select>	
+
+		<br>
+		<br>
+
+		<select name = "State" required> 
+		<?php 
+			$conn = mysql_connect("localhost","compuser","yeahsure");
+			mysql_select_db("4400DatabaseProject",$conn);
+			echo mysql_error();
+
+			if (!$conn) {
+				echo "Unable to connect to DB: " .mysql_error();
+				exit;
+			}
+
+			$theQuery = "select state FROM 'CityState'";
+			$theResponse = mysql_query($theQuery);
+
+			while($row  = mysql_fetch_assoc($theResponse)) {
+				echo "<option>".$row["state"]."</option>";
+			}
+		?>
+		</select>
+
+
+		<h3>Zip code: </h3><br>
+		<input type = "text" name = "zipCode"><br>
+
+	</form>	
+	
+	<script>
+        document.getElementById('errorMsg').innerHTML = localStorage.getItem("didFail");
+        localStorage.setItem("didFail","");
+    </script>
+
+
+	<form action="index.html">
+		<input type="submit" name="Log out">
+	</form>
+
+</html>
