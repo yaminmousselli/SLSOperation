a2bc78c441e43a9a071accf5770ee4fec78ff6a6
bedelman3 <bedelman3@gatech.edu>
2017-04-11 14:15:55 -0400

    All the current registration.php dependent js functions
    its great

--- js/registration.js
+++ js/registration.js
@@ -1,3 +1,27 @@
 function goToLogin() {
   window.location.assign("index.html");
 }
+
+var useType = document.getElementById('usrType');
+
+function showOrHide(){
+    if (useType.value == "City Official") {
+        document.getElementById("offStateSelect").style.display = "none";
+        document.getElementById("offCitySelect").style.display = "none";
+    }
+    if(useType.value == "City Scientist") {
+        document.getElementById("offStateSelect").style.display = "block";
+        document.getElementById("offCitySelect").style.display="block";
+    }
+}
+
+var pass = document.getElementById("pass");
+var confPass = document.getElementById('passConf');
+
+function validatePass() {
+  if (pass.value != confPass.value){
+      confPass.setCustomValidity("Passwords donut match");
+  } else {
+    confPass.setCustomValidity('');
+  }
+}
