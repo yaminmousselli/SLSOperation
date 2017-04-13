574765e24bc9b86aea7d541755364676c4edb185
bedelman3 <bedelman3@gatech.edu>
2017-04-13 18:58:24 -0400

    typo fix
    it works now

---
+++ functionality_official.js
@@ -0,0 +1,11 @@
+function goToFilterPoi() {
+    window.location.assign("SearchPOI.php")
+}
+
+function goToPoiReport() {
+    window.location.assign("PoiReport.php")
+}
+
+function goToLogin() {
+    window.location.assign("index.html")
+}
