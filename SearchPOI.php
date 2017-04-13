c0980b41f3ef8b251fef1cf799bdf8a20f1d47c3
bedelman3 <bedelman3@gatech.edu>
2017-04-13 17:13:20 -0400

    You can now search POIs!
    Should have full functionality, afaik.
    
    Haven't been able to test flagged points just because we don't have any in the db, so when we bugtest we should check that.

--- SearchPOI.php
+++ SearchPOI.php
@@ -6,7 +6,9 @@
 
     <script type="text/javascript" src="js/search_poi.js"></script>
     </head>
-    
+    <?php
+    include "dbConn.php";
+    ?>
     <header>Operation SLS</header>
     
     <div id="container">
@@ -16,20 +18,47 @@
                 <h3>View POIs</h3>
             </div>
             
-            <form action="SearchPOI.php" method="post">  
+            <form action="" method="post">  
                 <div class="viewEntry">
                     <label for="poiNameSelect">POI Location Name</label>
-                    <select id="poiNameSelect" name="poiNameSelect"></select>
+                    <select id="poiNameSelect" name="poiNameSelect">
+                    <?php
+
+                    $theQuery = "SELECT locationName FROM `Poi`";
+                    $theResponse = mysql_query($theQuery);
+
+                    while($row = mysql_fetch_assoc($theResponse)) {
+                        echo "<option>".$row["locationName"]."</option>";
+                    }
+                    ?>
+                    </select>
                 </div>
 
                 <div class="viewEntry">
                     <label for="citySelect">City</label>
-                    <select id="citySelect" name="citySelect"></select>
+                    <select id="citySelect" name="citySelect">
+                    <?php
+
+                    $theQuery = "SELECT city FROM CityState";
+                    $theResponse = mysql_query($theQuery);
+
+                    while ($row = mysql_fetch_assoc($theResponse)) {
+                        echo "<option>".$row["city"]."</option>";
+                    }
+                    ?></select>
                 </div>
 
                 <div class="viewEntry">
                     <label for="stateSelect">State</label>
-                    <select id="stateSelect" name="stateSelect"></select>
+                    <select id="stateSelect" name="stateSelect">
+                    <?php
+
+                        $theQuery = "SELECT DISTINCT state FROM CityState";
+                        $theResponse = mysql_query($theQuery);
+                        while ($row = mysql_fetch_assoc($theResponse)) {
+                            echo "<option>".$row["state"]."</option>";
+                        }
+                    ?></select>
                 </div>
 
                 <div class="viewEntry">
@@ -44,7 +73,7 @@
 
                 <div class="viewEntry" id="dateFlaggedEntry">
                     <label for="date">Date Flagged</label>
-                    <input type="date" id="dateFlaggedStart" name="dateFlagedStart"> <p>to</p> <input type="date" id="dateFlagedEnd" name="dateFlagedEnd"> 
+                    <input type="date" id="dateFlaggedStart" name="dateFlaggedStart"> <p>to</p> <input type="date" id="dateFlaggedEnd" name="dateFlaggedEnd"> 
                 </div>
 
                 <div id="filterControls">
@@ -58,6 +87,79 @@
         </div>
 
 
+        <div id="result">
+            <script>
+            
+            function goPoiDetail(thePoi){
+                localStorage.setItem("ourPoi",thePoi);
+                window.location.assign("PoiDetail.php");
+            }
+            </script>
+            
+            <script src="js/sorttable.js"></script>
+            <table class="sortable">
+                <tr>
+                    <th>Location Name</th>
+                    <th>State</th>
+                    <th>City</th>
+                    <th>Zip code</th>
+                    <th>Flagged</th>
+                    <th>Date Flagged</th>
+                </tr>
+            
+            
+            
+        
+            <?php
+            
+
+            
+            if (count($_POST) != 0){
+                
+                
+                
+                if($_POST['isFlagged'] == 1){
+                    $theQuery = "SELECT locationName, city, state, zipcode, flagged, dateFlagged FROM Poi 
+                    WHERE locationName = '".$_POST['poiNameSelect']."' AND
+                    city = '".$_POST['citySelect']."' AND
+                    state = '".$_POST['stateSelect']."' AND
+                    zipcode = '".$_POST['zipCode']."' AND
+                    flagged = '".$_POST['isFlagged']."' AND
+                    dateFlagged BETWEEN '".$_POST['dateFlaggedStart']."' AND '".$_POST['dateFlaggedEnd']."'";
+                }
+                
+                if($_POST['isFlagged'] == 0){
+                    $theQuery = "SELECT locationName, city, state, zipcode, flagged, dateFlagged FROM Poi 
+                    WHERE locationName = '".$_POST['poiNameSelect']."' AND
+                    city = '".$_POST['citySelect']."' AND
+                    state = '".$_POST['stateSelect']."' AND
+                    zipcode = '".$_POST['zipCode']."' AND
+                    flagged = '0'";
+                }
+                
+                echo $theQuery;
+                $theResponse = mysql_query($theQuery);
+                while ($row = mysql_fetch_assoc($theResponse)) {
+                    if($row['flagged'] == 0){
+                        $row['flagged'] = 'no';
+                        $row['dateFlagged'] = 'N/A';
+                    }
+                    echo "<tr><td><button onclick=\"goPoiDetail('".$row['locationName']."')\">".$row['locationName']."</button></td><td>".$row['state']."</td><td>".$row['city']."</td><td>".$row['zipcode']."</td><td>".$row['flagged']."</td><td>".$row['dateFlagged']."</td></tr>";
+                    
+                }
+                
+                
+                
+                
+                
+            }
+            
+            
+            
+            ?>
+            </table>
+        </div>
+        
     </div>
     
     
