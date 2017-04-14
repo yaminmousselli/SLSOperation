c3d3caf60878e7b9711aa5b4ee3a04b53f465899
Orestis.Mark <orestis.markozanes@gmail.com>
2017-04-13 22:44:45 -0400

    Halp plox

--- pendingCityOfficials.php
+++ pendingCityOfficials.php
@@ -1,11 +1,46 @@
+<html>
+
+<h1>Pending City Official Accounts</h1>
+<script src="js/sorttable.js"></script>
+<table class="sortable">
+    <tr>
+        <th>Username</th>
+        <th>Email</th>
+        <th>City</th>
+        <th>State</th>
+        <th>Title</th>
+        <th>Select</th>
+    </tr>
+
 <?php
-	include "dbConn.php";
-	$theQuery = "SELECT username FROM User";
-	$theResponse = mysql_query($theQuery);
-	
-	$concatLikeRows = array();
+    include "dbConn.php";
+    $theQuery = "SELECT User.username,email,city,state,title FROM User,CityOfficial WHERE User.username = CityOfficial.username GROUP BY username";
+    $theResponse = mysql_query($theQuery);
+    
+    $concatLikeRows = array();
     while ($row = mysql_fetch_assoc($theResponse)) {
-        	echo $row["username"];
-    	
-   	}	
+        echo '<tr>';
+        echo '<td>'.$row["username"].'</td>';
+        echo '<td>'.$row["email"].'</td>'; 
+        echo '<td>'.$row["city"].'</td>'; 
+        echo '<td>'.$row["state"].'</td>'; 
+        echo '<td>'.$row["title"].'</td>';   
+        echo '<td><input type="checkbox" name="'.ch1.'"value="unchecked"></td>';
+        echo '</tr>';
+    }   
+
 ?>
+</table>
+<form action = "ChooseFunctionalityOfficial.html">
+    <input type="submit" value="Back">
+</form>
+<br>
+<form action = "RejectCityOfficial.php"> <!--should delete selected CityOfficials -->
+    <input type = "submit" value = "Reject">
+<form>
+<br>
+<form action = "AcceptCityOfficial.php"> <!--should alter isApproved value for
+CityOfficials -->
+    <input type = "submit" value = "Accept">
+</form>
+</html>
