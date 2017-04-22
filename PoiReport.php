f5727189fefcf9ef03f931d58b7e1cf4d5d50026
bedelman3 <bedelman3@gatech.edu>
2017-04-22 14:23:17 -0400

    PoiReport Fixed
    You can sort correctly
    You can go back
    You can see all the POI locations
    YOU CAN HAVE IT ALL

--- PoiReport.php
+++ PoiReport.php
@@ -2,25 +2,24 @@
 
 
 <h1>Poi Report</h1>
-<script src="js/sorttable.js"></script>
-<table class="sortable">
+<script src="js/sortthetable.js"></script>
+<script src="js/poi_report.js"></script>
+<table id="ReportTable">
     <tr>
-        <th>Location</th>
-        <th>State</th>
-        <th>City</th>
-        <th>Mold Min</th>
-        <th>Mold Avg</th>
-        <th>Mold Max</th>
-        <th>AQ Min</th>
-        <th>AQ Avg</th>
-        <th>AQ Max</th>
-        <th># of data points</th>
-        <th>Flagged?</th>
+        <th onclick="sortTable(0)">Location</th>
+        <th onclick="sortTable(1)">State</th>
+        <th onclick="sortTable(2)">City</th>
+        <th onclick="sortTable(3)">Mold Min</th>
+        <th onclick="sortTable(4)">Mold Avg</th>
+        <th onclick="sortTable(5)">Mold Max</th>
+        <th onclick="sortTable(6)">AQ Min</th>
+        <th onclick="sortTable(7)">AQ Avg</th>
+        <th onclick="sortTable(8)">AQ Max</th>
+        <th onclick="sortTable(9)"># of data points</th>
+        <th onclick="sortTable(10)">Flagged?</th>
     </tr>
     <?php
-    
-    ini_set('display_errors', 'On');
-    error_reporting(E_ALL | E_STRICT);
+
     
     include "dbConn.php";
     /*echo "oh sheesh";
@@ -72,7 +71,15 @@
             array_push($concatLikeRows,$row);
         }
     }
+    
     $c = $concatLikeRows;
+    
+    
+    /*
+    $arrlength = count($c);
+    for($x = 0; $ < $arrlength; X++){}
+    */
+    
     
     $theQuery = "SELECT locationName, city, state, flagged FROM Poi";
     $theResponse = mysql_query($theQuery);
@@ -108,17 +115,24 @@
     $arrlength = count($c);
     for($x = 0; $x < $arrlength; $x++){
 
-        if(count($c[$x]) == 13){//I should figure out why $c[$x] has 13 attributes but whateva
-            echo "<tr><td>".$c[$x]['locationName']."</td><td>".$c[$x]['city']."</td><td>".$c[$x]['state']."</td><td>".$c[$x]['minMold']."</td><td>".$c[$x]['avgMold']."</td><td>".$c[$x]['maxMold']."</td><td>".$c[$x]['minAQ']."</td><td>".$c[$x]['avgAQ']."</td><td>".$c[$x]['maxAQ']."</td><td>".$c[$x]['numPoints']."</td><td>".$c[$x]['flagged']."</td></tr>";
-        }
+        //if(count($c[$x]) == 13){//I should figure out why $c[$x] has 13 attributes but whateva
+        echo "<tr><td>".$c[$x]['locationName']."</td><td>".$c[$x]['city']."</td><td>".$c[$x]['state']."</td><td>".$c[$x]['minMold']."</td><td>".$c[$x]['avgMold']."</td><td>".$c[$x]['maxMold']."</td><td>".$c[$x]['minAQ']."</td><td>".$c[$x]['avgAQ']."</td><td>".$c[$x]['maxAQ']."</td><td>".$c[$x]['numPoints']."</td><td>".$c[$x]['flagged']."</td></tr>";
+        //}
     }
     
     
     ?>
+    
+    
+
+    
     </table>
 <form action = "ChooseFunctionalityOfficial.html">
     <input type="submit" value="Back">
 </form>
+
+
+
 </html>
     
     
