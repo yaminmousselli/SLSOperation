580a626b9b54f99a691a184c5084f5e3645f7e9f
Jack Mueller <mueller.o.jack@gmail.com>
2017-04-22 00:37:37 -0400

    Correct city official login functionality. Improve login page queries.

--- logIn.php
+++ logIn.php
@@ -1,78 +1,45 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<script>
-function goCityScientist() {
-    window.location.assign("AddDataPoint.php")
-}
-
-function goAdmin() {
-    window.location.assign("ChooseFunctionalityAdmin.html")
-}
-
-function goCityOfficial() {
-    window.location.assign("ChooseFunctionalityOfficial.html")
-}
-</script>
-
 <?php
-
-//echo "trying<br>";
-
 include "dbConn.php";
 
-//$theQuery = "SELECT * FROM User WHERE username ='".$_POST['username']."' AND password = '".$POST_['password']."'";
+// Find the user that tried to login
+$usernameEntered = $_POST["username"];
+$passwordEntered = $_POST["password"];
 
-$theQuery = "SELECT * FROM User";
+$usersBasic = "SELECT username, password, userType FROM `User` WHERE `username` = '$usernameEntered' AND `password` = '$passwordEntered'";
+$users = mysql_query($usersBasic);
 
-//echo "this is the query: ".$theQuery."<br>";'''
+if ($user = mysql_fetch_assoc($users)) {
+    // If the user exists, go to the correct landing page
 
-$checkEmpty = mysql_query($theQuery);
+    $type = $user["userType"];
 
-echo mysql_error();
+    if ($type == "cityScientist") {
+        header("location:AddDataPoint.php");
+    }
 
+    if ($type == "cityOfficial") {
+        // If an official is not approved, don't permit log in
+        $officialApproved = "SELECT isApproved FROM `CityOfficial` WHERE `username` = '$usernameEntered'";
+        $officials = mysql_query($officialApproved);
+        $official = mysql_fetch_assoc($officials);
 
-//echo "<br>the query returned: ".$checkEmpty."<br>";
-
-$login = false;
-
-while($row = mysql_fetch_assoc($checkEmpty)) {
-
-
-    //echo "tried while<br>";
-    if($row["username"] == strtolower($_POST["username"])) {
-        if($row["password"] == $_POST["password"]){
-            $login = true;
-
+        if ($official["isApproved"] === NULL) {
+            // Value is NULL, admin hasn't set it yet
+            echo "City official not yet approved. Return to log in?";
+        } else if (!$official["isApproved"]) {
+            // Value is not NULL, but is falsy
+            echo "City official was rejected. Sorry. Return to log in?";
+        } else {
+            // Value is Truthy
+            header("location:ChooseFunctionalityOfficial.html");
         }
     }
-}
 
-if($login){
-    $theResponse = mysql_query("SELECT userType FROM User WHERE username='".$_POST["username"]."'");
-    $theirType = mysql_fetch_assoc($theResponse)["userType"];
-
-    if($theirType == "cityScientist") {
-        echo "<script>goCityScientist();</script>";
+    if ($type == "admin") {
+        header("location:ChooseFunctionalityAdmin.html");
     }
-
-    if($theirType == "cityOfficial") {
-        echo "<script>goCityOfficial();</script>";
-    }
-
-    if($theirType == "admin") {
-        echo "<script>goAdmin();</script>";
-    }
-}
-
-if(!$login){
+} else {
     echo "Login failed. Try again?";
-    echo '<form action="index.html"><input type="submit" value="Back" /></form>';
-
 }
-//echo "tried";
-
+echo '<form action="index.html"><input type="submit" value="Back" /></form>';
 ?>
-  </body>
-</html>
