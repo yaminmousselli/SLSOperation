7ac8dc2d32282deaf11086fc29d4b3727fe24852
Yamin <ymousselli@gmail.com>
2017-04-22 00:10:44 -0400

    Optimized userAdd.php and deleted SearchPOI.php

--- AcceptCityOfficial.php
+++ AcceptCityOfficial.php
@@ -1,40 +1,37 @@
 <html>
 <script>
-    function pointFailed(){
+    function pointFailed() {
         localStorage.setItem("didFail","Nothing was selected");
         window.location.assign("pendingCityOfficials.php")
     }
-    function pointAccepted(){
-
-            localStorage.setItem("didFail","CityOfficials were approved");
+    function pointAccepted() {
+        localStorage.setItem("didFail","CityOfficials were approved");
         window.location.assign("pendingCityOfficials.php")
     }
-    function pointRejected(){
-            localStorage.setItem("didFail","CityOfficials were rejected");
+    function pointRejected() {
+        localStorage.setItem("didFail","CityOfficials were rejected");
         window.location.assign("pendingCityOfficials.php")
     }
 </script>
 
-<?php 
+<?php
     include "dbConn.php";
 
     $selected = $_POST['ch'];
     if ($_POST['action'] == 'Accept') {
         foreach ($selected as $value) {
-            
-            $theQuery = "UPDATE CityOfficial SET isApproved = 1 WHERE username = '$value'";     
+
+            $theQuery = "UPDATE CityOfficial SET isApproved = 1 WHERE username = '$value'";
             $theResponse = mysql_query($theQuery);
-            echo mysql_error(); 
+            echo mysql_error();
         }
         if ($theResponse) {
             echo "<script>pointAccepted();</script>";
         }
-
     }
     if ($_POST['action'] == 'Reject') {
         foreach ($selected as $value) {
-            
-            $theQuery = "UPDATE CityOfficial SET isApproved = 0 WHERE username = '$value'";     
+            $theQuery = "UPDATE CityOfficial SET isApproved = 0 WHERE username = '$value'";
             $theResponse = mysql_query($theQuery);
             echo mysql_error();
         }
@@ -43,7 +40,7 @@
         }
     }
         if(!$theResponse){
-                echo "<script>pointFailed();</script>"; 
+                echo "<script>pointFailed();</script>";
             }
     //should alter the isApproved value in CityOfficials
  ?>
